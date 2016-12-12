dbd674eb09109151e3c67d4eb4a5a5aafd35b992
pooledesign <benp@pooledesign.net>
2016-12-12 16:06:48 -0600

    response.write

--- index.aspx.cs
+++ index.aspx.cs
@@ -6,6 +6,7 @@ public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        response.write('ddd');
         lbl1.Text = DateTime.Now.ToString();
     }
 }
