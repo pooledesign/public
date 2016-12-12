2401cf43866d4e6fd15e33e82c56618ad999818d
pooledesign <benp@pooledesign.net>
2016-12-12 15:35:04 -0600

    debugging error.

--- index.aspx.cs
+++ index.aspx.cs
@@ -2,6 +2,6 @@ public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        lbl1.Text = DateTime.Now.ToString();
+        //lbl1.Text = DateTime.Now.ToString();
     }
 }
